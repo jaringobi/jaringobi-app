ac75af519ea8ddfc41fa23e4bb906e23f424848e
HamBeomJoon <37996727+HamBeomJoon@users.noreply.github.com>
2024-05-23 22:53:58 +0900

    feat | splash, 시작 페이지 (#5)
    * chore | 폰트스타일, color, logo 설정
    
    * feat | splash페이지
    
    * feat | start페이지, dialog
    
    * chore | 변수명 수정
    
    * Update app/src/main/res/layout/activity_start.xml
    
    Co-authored-by: Bokyeom <79684339+k-kbk@users.noreply.github.com>
    
    * Update app/src/main/res/layout/dialog_select_goal.xml
    
    Co-authored-by: Bokyeom <79684339+k-kbk@users.noreply.github.com>
    
    * Update app/src/main/res/values/strings.xml
    
    Co-authored-by: Bokyeom <79684339+k-kbk@users.noreply.github.com>
    
    * Update app/src/main/res/values/font_style.xml
    
    Co-authored-by: Bokyeom <79684339+k-kbk@users.noreply.github.com>
    
    * Update app/src/main/res/values/strings.xml
    
    Co-authored-by: Bokyeom <79684339+k-kbk@users.noreply.github.com>
    
    * Update app/src/main/res/drawable/dialog_round.xml
    
    Co-authored-by: Bokyeom <79684339+k-kbk@users.noreply.github.com>
    
    * Update app/src/main/java/com/example/jaringobi/StartDialog.kt
    
    Co-authored-by: Bokyeom <79684339+k-kbk@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Bokyeom <79684339+k-kbk@users.noreply.github.com>

---
+++ app/src/main/java/com/example/jaringobi/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.example.jaringobi
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.jaringobi.databinding.ActivitySplashBinding
+
+@Suppress("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        // 저장된 정보 있으면 메인으로, 아니면 시작 페이지로
+        moveActivity(StartActivity())
+    }
+
+    private fun moveActivity(p: Activity) {
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, p::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
+    }
+}

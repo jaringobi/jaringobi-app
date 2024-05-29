a6a6d5c319b33a00168d7af592093d801e55ff91
k-kbk <kkbk0077@gmail.com>
2024-05-29 13:50:09 +0900

    feat | 영수증 업로드 레이아웃

---
+++ app/src/main/java/com/example/jaringobi/view/addExpensePage/ReceiptFragment.kt
@@ -0,0 +1,18 @@
+package com.example.jaringobi.view.addExpensePage
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.jaringobi.databinding.FragmentReceiptBinding
+
+class ReceiptFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return FragmentReceiptBinding.inflate(inflater, container, false).root
+    }
+}

f59a1528d9edd95fde4eed767f120621db07ae63
k-kbk <kkbk0077@gmail.com>
2024-06-01 20:39:54 +0900

    chore | 디자인 수정

--- app/src/main/java/com/example/jaringobi/view/expenseListPage/CalendarAdapter.kt
+++ app/src/main/java/com/example/jaringobi/view/expenseListPage/CalendarAdapter.kt
@@ -20,12 +20,18 @@ class CalendarAdapter(
 ) : RecyclerView.Adapter<CalendarAdapter.CalendarViewHolder>() {
     private var selectedPosition = RecyclerView.NO_POSITION
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CalendarViewHolder {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int,
+    ): CalendarViewHolder {
         val binding = ItemCalendarDayBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return CalendarViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: CalendarViewHolder, position: Int) {
+    override fun onBindViewHolder(
+        holder: CalendarViewHolder,
+        position: Int,
+    ) {
         holder.bind(dates[position], position)
     }
 
@@ -36,14 +42,16 @@ class CalendarAdapter(
      */
     inner class CalendarViewHolder(private val binding: ItemCalendarDayBinding) :
         RecyclerView.ViewHolder(binding.root) {
-
         /**
          * 날짜를 TextView에 바인딩하고 클릭 리스너를 설정
          *
          * @param date 바인딩할 날짜.
          * @param position 어댑터에서 항목의 위치.
          */
-        fun bind(date: Int, position: Int) {
+        fun bind(
+            date: Int,
+            position: Int,
+        ) {
             binding.calendarDayText.text = date.toString()
             setWeekendColor(binding.calendarDayText, position)
             setSelectionBackground(position)
@@ -61,7 +69,8 @@ class CalendarAdapter(
          */
         private fun setSelectionBackground(position: Int) {
             if (selectedPosition == position) {
-                binding.calendarDayText.background = ContextCompat.getDrawable(binding.root.context, R.drawable.selected_date_background)
+                binding.calendarDayText.background =
+                    ContextCompat.getDrawable(binding.root.context, R.drawable.selected_date_background)
             } else {
                 binding.calendarDayText.background = null
             }
@@ -73,13 +82,17 @@ class CalendarAdapter(
          * @param textView 색상을 설정할 TextView.
          * @param position 어댑터에서 항목의 위치.
          */
-        private fun setWeekendColor(textView: TextView, position: Int) {
+        private fun setWeekendColor(
+            textView: TextView,
+            position: Int,
+        ) {
             val dayOfWeek = (position + 1) % 7
-            val colorResId = when (dayOfWeek) {
-                0 -> R.color.sunday_red
-                6 -> R.color.saturday_blue
-                else -> R.color.default_text_color
-            }
+            val colorResId =
+                when (dayOfWeek) {
+                    0 -> R.color.sunday_red
+                    6 -> R.color.saturday_blue
+                    else -> R.color.default_text_color
+                }
             textView.setTextColor(ContextCompat.getColor(textView.context, colorResId))
         }
 

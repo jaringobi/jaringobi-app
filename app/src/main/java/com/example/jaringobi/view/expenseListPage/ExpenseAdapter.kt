83718473aa9ad55cc3e936730f0ca125c9d9d0a4
kay25802 <119282789+kay25802@users.noreply.github.com>
2024-06-02 11:01:28 +0900

    지출 내역 목록 수정하기 기능 추가 (#28)
    * 지출 내역 목록 페이지
    
    * 지출 내역 수정 기능 구현
    
    ---------
    
    Co-authored-by: KimAYoung <dalmadbb@gmail.com>

--- app/src/main/java/com/example/jaringobi/view/expenseListPage/ExpenseAdapter.kt
+++ app/src/main/java/com/example/jaringobi/view/expenseListPage/ExpenseAdapter.kt
@@ -8,21 +8,24 @@ import com.example.jaringobi.data.db.ExpenseEntity
 import com.example.jaringobi.databinding.ItemExpenseBinding
 
 class ExpenseAdapter(
-    private var expenses: List<ExpenseEntity>,
-    private val onDeleteClick: (ExpenseEntity) -> Unit,
+    private var expenses: MutableList<ExpenseEntity>,
+    private val onEditClick: (ExpenseEntity) -> Unit,
+    private val onDeleteClick: (ExpenseEntity) -> Unit
 ) : RecyclerView.Adapter<ExpenseAdapter.ExpenseViewHolder>() {
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int,
-    ): ExpenseViewHolder {
+
+    // 데이터를 업데이트하고 어댑터에 변경 사항을 알리는 메서드 추가
+    fun updateExpenses(newExpenses: List<ExpenseEntity>) {
+        expenses.clear()
+        expenses.addAll(newExpenses)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExpenseViewHolder {
         val binding = ItemExpenseBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ExpenseViewHolder(binding)
     }
 
-    override fun onBindViewHolder(
-        holder: ExpenseViewHolder,
-        position: Int,
-    ) {
+    override fun onBindViewHolder(holder: ExpenseViewHolder, position: Int) {
         holder.bind(expenses[position])
     }
 
@@ -30,20 +33,22 @@ class ExpenseAdapter(
 
     inner class ExpenseViewHolder(private val binding: ItemExpenseBinding) :
         RecyclerView.ViewHolder(binding.root) {
+
         fun bind(expense: ExpenseEntity) {
             binding.expenseDate.text = expense.date
             binding.expenseName.text = expense.store
-            binding.expenseAmount.text = expense.cost
-            // 삭제 버튼 클릭 리스너 설정
+            binding.expenseAmount.text = expense.cost.toString()
+
             binding.btnDelete.setOnClickListener {
-                onDeleteClick(expense) // 삭제 콜백 호출
-                // UI 즉시 업데이트: 클릭된 항목을 리스트에서 제거
-                expenses = expenses.filter { it.id != expense.id }
+                onDeleteClick(expense)
+                expenses = expenses.filter { it.id != expense.id }.toMutableList()
                 notifyItemRemoved(adapterPosition)
                 notifyItemRangeChanged(adapterPosition, expenses.size)
-
-                // 삭제 시 토스트 메시지 표시
                 Toast.makeText(binding.root.context, "삭제 완료", Toast.LENGTH_SHORT).show()
+            }
+
+            binding.btnEdit.setOnClickListener {
+                onEditClick(expense)
             }
         }
     }
